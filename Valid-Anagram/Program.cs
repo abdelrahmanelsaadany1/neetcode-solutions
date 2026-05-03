28f0c98602835ee2106c78bfc7ad4646d974dbae
Abdelrahman Elsaadany <abdelrahmanelsaadany18@gmail.com>
2026-05-04 01:59:15 +0300

    second problem

---
+++ Valid-Anagram/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace Valid_Anagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public class Solution
+        {
+            public bool IsAnagram(string s, string t)
+            {
+                if (s.Length !=t.Length ) return false;
+                Dictionary<char,int> D = new Dictionary<char,int>();
+                foreach (char letter in s)
+                {
+                    if (D.ContainsKey(letter))
+                    {
+                        D[letter] += 1;
+
+                        
+                    }
+                    else
+                    {
+                        D[letter] = 1;
+                    }
+                }
+                foreach (var letter in t)
+                {
+                    if (D.ContainsKey(letter))
+                    {
+                        D[letter]--;
+                    }
+                    else
+                    {
+                        D[letter]=1;
+                    }
+                    
+                }
+                foreach (var Value in D.Values)
+                {
+                    if (Value !=0)
+                    {
+                        return false;
+                        
+                    }
+
+                }
+                return true;
+
+
+
+
+            }
+        }
+
+    }
+}
