e90afb6222b5733c0d9f8034ddd6b4542ff6d3ef
Abdelrahman Elsaadany <abdelrahmanelsaadany18@gmail.com>
2026-05-04 01:24:46 +0300

    first commit

---
+++ NeetCode/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace NeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public bool hasDuplicate(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (!set.Add(num))
+                {
+                    return true;
+
+                }
+
+            }
+            return false;
+
+        }
+    }
+}
