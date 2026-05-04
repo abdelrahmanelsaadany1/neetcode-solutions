60d656fa1809d7d4a7c015877a9715c700d79f68
Abdelrahman Elsaadany <abdelrahmanelsaadany18@gmail.com>
2026-05-04 16:01:54 +0300

    third deleted project

---
+++ HasDuplicate/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HasDuplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public class Solution
+        {
+            public bool hasDuplicate(int[] nums)
+            {
+                HashSet<int> set = new HashSet<int>();
+                foreach (var num in nums)
+                {
+                    if (!set.Add(num))
+                    {
+                        return true;
+
+                    }
+
+                }
+                return false;
+
+            }
+        }
+    }
+}
